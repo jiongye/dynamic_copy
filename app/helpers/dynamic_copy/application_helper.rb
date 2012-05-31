886e86f43520ba1bfa9291ee2e565f4ebba711fc
Jiongye Li <jiongye@gmail.com>
2012-05-31 17:02:11 -0400

    added more UI styling

--- app/helpers/dynamic_copy/application_helper.rb
+++ app/helpers/dynamic_copy/application_helper.rb
@@ -3,7 +3,7 @@ module DynamicCopy
 
     def t(*args)
       output = I18n.translate(*args)
-      output << content_tag(:span, link_to('translate', dynamic_copy.edit_translation_path(:id => args.shift), :class => 'edit_translation')) if session[:show_translation_link]
+      output << content_tag(:span, link_to('translate', dynamic_copy.edit_translation_path(:id => args.shift), :class => 'edit_translation'), :class => 'dynamic_copy') if session[:show_translation_link]
       output
     end
 
